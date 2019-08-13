9f245203e586f93875881808c69292ccf2b3a717
yuting hou <hou00023@algonquinlive.com>
2019-08-13 14:08:06 -0400

    add social

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ function click() {
     }
 }
 
+function contact(){
+    document.querySelector(".left li:nth-child(4)").dispatchEvent(new MouseEvent('click'));
+}
+
 function hidePage() {
     document.querySelectorAll("main section").forEach(function (page) {
         page.classList.add("hide");
